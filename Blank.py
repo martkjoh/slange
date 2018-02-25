7c76f1ab9519b25bca9f4302d371d0ed55d7f7e2
Martin <martin.johnsrud@gmail.com>
2018-02-25 12:13:46 +0100

    working

--- Blank.py
+++ Blank.py
@@ -1,14 +1,23 @@
 from matplotlib import pyplot as plt
 import numpy as np
 
-x = np.linspace(0, 100, 100)
-y = x**2
-z = np.sqrt(x)
 
-fig = plt.figure()
-ax = fig.add_subplot(121)
-ax.plot(x, y)
-ax = fig.add_subplot(122)
-ax.plot(x, z)
+def polar_transform(r, theta):
+    return r * np.cos(theta), r * np.sin(theta)
 
+
+def r(theta):
+    return 6 * np.cos(theta)
+
+
+def r_2(theta):
+    return 5 / np.cos(theta)
+
+
+fig, ax = plt.subplots(1)
+ax.set_xlim(-1, 8)
+ax.set_ylim(-5, 5)
+theta = np.linspace(-np.arccos(np.sqrt(5/6)), np.arccos(np.sqrt(5/6)), 1000)
+plt.plot(*polar_transform(r(theta), theta))
+plt.plot(*polar_transform(r_2(theta), theta))
 plt.show()
