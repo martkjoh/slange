17fd45ddb470c8929297c471b8c5cb08ee26ae49
Martin <martin.johnsrud@gmail.com>
2018-04-17 20:20:41 +0200

    Kjemi ass

--- Kjemi/lab8.py
+++ Kjemi/lab8.py
@@ -1,16 +1,16 @@
 import matplotlib.pyplot as plt
-import numpy as numpy
+import numpy as np
 
 
 fig=plt.figure()
-
+V_0 = 0
 ################################### Experimental data
-Volum = [15.7, 20.4, 26, 31.6, 36.8, 42.2, 47.1]  # ml
-Volum = [v-Volum[0] for v in Volum]
-Potential = [-131, -129, -115, 0, 180, 190, 204]  # mV
+volum, potensial = np.loadtxt("data.txt", skiprows=1, unpack=True)
+volum -= V_0
 
 ################################### Plot
-plt.plot(Volum, Potential, "-x", marker="x", markeredgecolor="k")
+plt.figure()
+plt.plot(volum, potensial, "-x", marker="x", markeredgecolor="k")
 
 ################################### save plot
 plt.ylabel('$E$ [mV]')
