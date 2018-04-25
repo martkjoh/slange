b7b188f1b5f4b084d9f222f911b12c04c0e4422a
Martin <martin.johnsrud@gmail.com>
2018-04-25 13:15:24 +0200

    Mer opprydning

--- main/Drikke/plup p min.py
+++ main/Drikke/plup p min.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 x_dag, x_tid, y = np.loadtxt("C:\\Users\\Martin\\Google Drive\\Mat og drikke\\--navn kommer--v.2\\plupp pr sek.tsv", comments="#", unpack=True, dtype=str)
 
 y = np.array(y, dtype="float32")
@@ -9,5 +10,10 @@ x_tid = np.array([a.split(":") for a in x_tid])
 x_tid = np.array([int(a[0]) * 60 + int(a[1]) for a in x_tid])
 x = 24*60 * x_dag + x_tid
 
+a = np.polyfit(x, y, 3)
+
+x_2 = np.linspace(0, x[-1], 100)
+y_2 = a[3] + a[2]*x_2 + a[1]*x_2**2 + a[0]*x_2**3
 plt.plot(x, y, "-x")
+plt.plot(x_2, y_2)
 plt.show()
--- main/linalg.py
+++ main/linalg.py
@@ -99,11 +99,40 @@ def print_wolframalpha(A):
     x = x[:-2] + "]\n"
     print(x)
 
+def lineær_regresjon(x, y, y_func=lambda x: x, values=False):
 
-n = 3
-m = 5
+    x_norm = y_func(x)
+    N = len(x_norm)
+    S_x = np.sum(x_norm)
+    S_y = np.sum(y)
+    S_xx = np.sum(x_norm ** 2)
+    S_xy = np.sum(x_norm * y)
+    delta = N * S_xx - S_x ** 2
+    a_0 = (S_y * S_xx - S_x * S_xy) / delta
+    a_1 = (N * S_xy - S_x * S_y) / delta
 
-A = np.random.randint(0, 10, size=(n, m)).astype("float64")
-B =row_reduce(copy.deepcopy(A))
-print(sympy.Matrix(A).rref())
-print(B)
+    def f(x):
+        return a_0 + a_1 * y_func(x)
+
+    # Avvik fra regresjonsmodellen
+    D_y = f(x) - y
+    S = np.sum(D_y ** 2)
+    # Standaravvik
+    Da_0 = np.sqrt(1 / (N - 2) * (S * S_xx) / delta)
+    Da_1 = np.sqrt(N / (N - 2) * S / delta)
+    print("a_0:", a_0, "a_1:", a_1)
+    print("Da_0:", Da_0, "Da_1:", Da_1)
+
+    if values:
+        return f, D_y, (a_0, a_1, N, delta, S_x, S_y, S_xx, S_xy)
+    return f, D_y
+
+
+if __name__ == "__main__":
+    n = 3
+    m = 5
+
+    A = np.random.randint(0, 10, size=(n, m)).astype("float64")
+    B =row_reduce(copy.deepcopy(A))
+    print(sympy.Matrix(A).rref())
+    print(B)
