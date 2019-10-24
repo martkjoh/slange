3cfaed7bbc88baeaaddc6fe0399c4c9dd8c80ad0
Martin <martin.johnsrud@gmail.com>
2019-10-24 19:32:16 +0200

    fixed bug

--- main/polarization.py
+++ main/polarization.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 
 # Jones-vector
 v = np.array([
-    2, 
-    6 + 8j
+    1j, 
+    1
 ])
 
 # Normalization
@@ -14,10 +14,13 @@ v = A * v
 
 # The difference in phase between the components of
 phi = np.angle(v[0]) - np.angle(v[1])
+
+# Finding the direction of polarization
 dirOfPol = ""
-if phi <= pi: dirOfPol = "positive"
+if phi >= 0: dirOfPol = "positive"
 else: dirOfPol = "negative"
 
+# Drawing the polarization diagram
 theta = np.linspace(0, 2*pi, 100)
 x = abs(v[0]) * cos(theta)
 y = abs(v[1]) * sin(theta - pi / 2 - phi)
@@ -26,5 +29,6 @@ fig, ax = plt.subplots()
 fig.suptitle("Ploarization of $\\vec v = ({:1.3f}, {:1.3f})$, in ".format(v[0], v[1]) + dirOfPol + " direction." )
 ax.set_xlim(-1, 1)
 ax.set_ylim(-1, 1)
+ax.grid()
 ax.plot(x, y)
 plt.show()
