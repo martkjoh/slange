b8e167ef1719cb10afc1cae56bbad09a53298ea6
Martin Johnsrud <martin.johnsrud@gmail.com>
2020-11-05 21:49:09 +0100

    investigated pion decay

--- røkla/pion_decay.py
+++ røkla/pion_decay.py
@@ -2,3 +2,12 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 
+x = np.linspace(0, 1, 100)
+y = np.linspace(0, 1.5, 100)
+x, y = np.meshgrid(x, y)
+
+fig = plt.figure()
+ax = Axes3D(fig)
+ax.plot_surface(x, y, x ** 2*(x ** 2 - y ** 2)** 2)
+
+plt.show()
