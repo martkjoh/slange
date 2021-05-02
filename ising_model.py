fff700e0cd797ee4d882bf2839da47f236a2cb09
Martin Johnsrud <martin.johnsrud@gmail.com>
2021-05-02 22:16:47 +0200

    wee

--- ising_model.py
+++ ising_model.py
@@ -2,12 +2,12 @@ import numpy as np
 from matplotlib import pyplot as plt
 from numba import njit
 from tqdm import trange
+from multiprocessing import Pool, cpu_count
 
-
-N = 10
-sweeps = 100
-equill = 1000
-num_samples = 100
+N = 16
+sweeps = 10
+equill = 10000
+num_samples = 10000
 J=1
 
 
@@ -31,6 +31,8 @@ def sweep(S, T):
 def mag(S, T):
     return np.abs(np.sum(S))/N**2
 
+def mag2(S, T):
+    return mag(S, T)**2
 
 def energy(S, T):
     return np.sum(J*S*(
@@ -43,34 +45,39 @@ def energy2(S, T):
 
 
 sample_funcs = {
-    "Magnetization" :   mag,
-    "Energy" :          energy,
-    "Energy2":          energy2,
+    "Mag" :     mag,
+    "Mag2" :    mag2,
+    "Energy" :  energy,
+    "Energy2":  energy2,
 }
 
 def get_samples(S, T, samples):
     for key in samples:
-        samples[key].append(
-            sample_funcs[key](S, T)
-        )
+        samples[key] += sample_funcs[key](S, T)
     return samples
 
 
-def simulate(T):
-    S = np.random.choice([-1, 1], (N, N))
+def simulate(T, num_samples):
+    print("T = {:.3f}".format(T))
+    # S = np.random.choice([-1, 1], (N, N))
+    S = np.ones((N, N))
     samples = {
-    "Magnetization" :   [],
-    "Energy" :          [],
-    "Energy2":          [],
+    "Mag" :     0,
+    "Mag2" :    0,
+    "Energy" :  0,
+    "Energy2":  0,
     }
 
     for _ in range(equill): sweep(S, T)
     i = 0
     while i<num_samples:
         for _ in range(sweeps): sweep(S, T)
-        samples = get_samples(S, T, samples)
+        get_samples(S, T, samples)
         i += 1
-    return S, samples
+    for key in samples:
+        samples[key] *= 1/num_samples
+    return samples
+
 
 # Ts = np.linspace(0.1, 5, 100)
 # avs = [np.sum(simulate(T))/N**2 for T in Ts]
@@ -82,23 +89,3 @@ def simulate(T):
 # Ss = [simulate(T) for T in Ts]
 # [ax[i].imshow(Ss[i]) for i in range(len(Ss))]
 # plt.show()
-
-Temps = 10
-Ts = np.linspace(0.1, 5, Temps)
-Es = np.empty(Temps)
-Ms = np.empty(Temps)
-Hc = np.empty(Temps)
-
-
-for i in range(Temps):
-    S, samples = simulate(Ts[i])
-    Es[i] = np.sum(samples["Energy"]) / num_samples
-    Ms[i] = np.sum(samples["Magnetization"]) / num_samples
-    Hc[i] = (np.sum(samples["Energy2"]) - np.sum(samples["Energy"])**2) / num_samples / Ts[i] 
-
-plt.plot(Ts, Ms, "kx")
-plt.show()
-plt.plot(Ts, Es, "kx")
-plt.show()
-plt.plot(Ts, Hc, "kx")
-plt.show()
